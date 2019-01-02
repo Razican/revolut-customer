5206bc86383a4271d7a4fdc2a0d3c2d8d706f6ab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-02 15:57:14 +0000

    Deploy Razican/revolut-customer to github.com/Razican/revolut-customer.git:gh-pages

--- implementors/failure/trait.Fail.js
+++ implementors/failure/trait.Fail.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["failure"] = [];
-implementors["revolut_customer"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"revolut_customer/error/struct.AmountParse.html\" title=\"struct revolut_customer::error::AmountParse\">AmountParse</a>",synthetic:false,types:["revolut_customer::error::AmountParse"]},{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"revolut_customer/error/enum.Api.html\" title=\"enum revolut_customer::error::Api\">Api</a>",synthetic:false,types:["revolut_customer::error::Api"]},];
+implementors["revolut_customer"] = [{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"struct\" href=\"revolut_customer/amount/struct.ParseError.html\" title=\"struct revolut_customer::amount::ParseError\">ParseError</a>",synthetic:false,types:["revolut_customer::amount::ParseError"]},{text:"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"revolut_customer/enum.ApiError.html\" title=\"enum revolut_customer::ApiError\">ApiError</a>",synthetic:false,types:["revolut_customer::ApiError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
